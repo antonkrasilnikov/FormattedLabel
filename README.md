60f89fc654ad95d43c5b219f03ac4b6cd5876281
Anton Krasilnikov <a.krasilnikov@incrdbl.me>
2023-04-04 11:09:02 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FormattedLabel
+
+A description of this package.
---
+++ Sources/FormattedLabel/FormattedLabel.swift
@@ -0,0 +1,6 @@
+public struct FormattedLabel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
